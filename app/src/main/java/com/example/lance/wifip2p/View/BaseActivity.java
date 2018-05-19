491c6413710138bfb77b06f607413f91bd26a33d
LanceShu <869924650@qq.com>
2018-05-19 22:20:04 +0800

    Add the function of open wifi and find wifi devices

---
+++ app/src/main/java/com/example/lance/wifip2p/View/BaseActivity.java
@@ -0,0 +1,86 @@
+package com.example.lance.wifip2p.View;
+
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.lance.wifip2p.Interface.Wifip2pActionListener;
+import com.example.lance.wifip2p.Receiver.Wifip2pReceiver;
+
+import java.util.Collection;
+
+/**
+ * Created by Lance
+ * on 2018/5/18.
+ */
+
+public class BaseActivity extends AppCompatActivity implements Wifip2pActionListener {
+
+    public WifiP2pManager wifiP2pManager;
+    public WifiP2pManager.Channel channel;
+    public Wifip2pReceiver wifip2pReceiver;
+    public WifiP2pInfo wifiP2pInfo;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // register WifiP2pManager;
+        wifiP2pManager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
+        channel = wifiP2pManager.initialize(this, getMainLooper(), this);
+        // register broadcast
+        wifip2pReceiver = new Wifip2pReceiver(wifiP2pManager, channel, this);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        registerReceiver(wifip2pReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(wifip2pReceiver);
+        wifip2pReceiver = null;
+    }
+
+    @Override
+    public void wifiP2pEnabled(boolean enabled) {
+        Log.e("BaseActivity", "传输通道是否可用：" + enabled);
+    }
+
+    @Override
+    public void onConnection(WifiP2pInfo wifiP2pInfo) {
+        if (wifiP2pInfo != null) {
+            this.wifiP2pInfo = wifiP2pInfo;
+            Log.e("BaseActivity", "WifiP2pInfo:" + wifiP2pInfo);
+        }
+    }
+
+    @Override
+    public void onDisconnection() {
+        Log.e("BaseActivity", "连接断开");
+    }
+
+    @Override
+    public void onDeviceInfo(WifiP2pDevice wifiP2pDevice) {
+        Log.e("BaseActivity", "当前的的设备名称: " + wifiP2pDevice.deviceName);
+    }
+
+    @Override
+    public void onPeersInfo(Collection<WifiP2pDevice> wifiP2pDeviceList) {
+        for (WifiP2pDevice device : wifiP2pDeviceList) {
+            Log.e("BaseActivity", "连接的设备信息：" + device.deviceName + "--------" + device.deviceAddress);
+        }
+    }
+
+    @Override
+    public void onChannelDisconnected() {
+
+    }
+}
--- app/src/main/java/com/example/lance/wifip2p/View/MainActivity.java
+++ app/src/main/java/com/example/lance/wifip2p/View/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.lance.wifip2p.View;
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.lance.wifip2p.Adapter.FileAdapter;
 import com.example.lance.wifip2p.Content;
@@ -22,15 +26,19 @@ import com.example.lance.wifip2p.DataBean.WordList;
 import com.example.lance.wifip2p.R;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private Handler postHandler = new Handler();
     public static List<FileBean> wordList;
+    private List<WifiP2pDevice> deviceList = new ArrayList<>();
+
     private ProgressDialog progressDialog;
     private RecyclerView fileList;
     private Button sendFileBtn;
+    private Button receiveFileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +58,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private void initWight() {
         fileList = findViewById(R.id.file_list);
         sendFileBtn = findViewById(R.id.send_bt);
+        receiveFileBtn = findViewById(R.id.receive_bt);
         progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Scanning...");
+        progressDialog.setMessage("Scanning files...");
         if (!progressDialog.isShowing()) {
             progressDialog.show();
         }
         sendFileBtn.setOnClickListener(this);
+        receiveFileBtn.setOnClickListener(this);
     }
 
     private void CheckPermission() {
@@ -126,7 +136,99 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 for (SendFileBean fileBean : Content.selectedFileList) {
                     Log.e("SendFileBean", fileBean.getSendName() + "  " + fileBean.getSendPath() + "  " + fileBean.getSendSize());
                 }
+                progressDialog.setMessage("Scanning devices...");
+                progressDialog.show();
+                findServer();
+                break;
+            case R.id.receive_bt:
+                createGroup();
                 break;
         }
+    }
+
+    // Find the all devices can shot wifi;
+    private void findServer() {
+        wifiP2pManager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.e("SendBtn", "搜索设备成功");
+            }
+
+            @Override
+            public void onFailure(int i) {
+                Log.e("SendBtn", "搜索设备失败");
+            }
+        });
+    }
+
+    // connect the device;
+    private void connectDevice(WifiP2pDevice wifiP2pDevice) {
+        WifiP2pConfig config = new WifiP2pConfig();
+        if (wifiP2pDevice != null) {
+            config.deviceAddress = wifiP2pDevice.deviceAddress;
+            config.wps.setup = WpsInfo.PBC;
+            wifiP2pManager.connect(channel, config, new WifiP2pManager.ActionListener() {
+                @Override
+                public void onSuccess() {
+                    Toast.makeText(MainActivity.this, "连接成功", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(int i) {
+                    Toast.makeText(MainActivity.this, "连接失败", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onPeersInfo(Collection<WifiP2pDevice> wifiP2pDeviceList) {
+        super.onPeersInfo(wifiP2pDeviceList);
+        for (WifiP2pDevice device : wifiP2pDeviceList) {
+            if (!deviceList.contains(device)) {
+                deviceList.add(device);
+            }
+        }
+        progressDialog.dismiss();
+        showDeviceInfo();
+    }
+
+    private void showDeviceInfo() {
+        if (deviceList != null) {
+            for (WifiP2pDevice device : deviceList) {
+                Log.e("设备: ", device.deviceName + "----" + device.deviceAddress);
+            }
+        }
+    }
+
+    // create the receiver's wifi group;
+    public void createGroup() {
+        wifiP2pManager.createGroup(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "创建群组成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int i) {
+                Toast.makeText(MainActivity.this, "创建群组失败,请移除已有的组群或者连接同一WIFI重试", Toast.LENGTH_SHORT).show();
+                removeGroup();
+            }
+        });
+    }
+
+    // remove the receiver's wifi group;
+    public void removeGroup() {
+        wifiP2pManager.removeGroup(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "移除群组成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int i) {
+                Toast.makeText(MainActivity.this, "移除组群失败,请创建组群重试", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
